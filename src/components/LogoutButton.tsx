fa922419909c542170833036b52440138a45351f
fasihu1996 <fasih1996@gmail.com>
2025-07-27 12:52:39 +0200

    init

---
+++ src/components/LogoutButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+import { LogOut } from "lucide-react";
+import { Button } from "./ui/button";
+
+export default function LogoutButton() {
+  const router = useRouter();
+  const supabase = createClient();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.refresh(); // This will trigger a server-side re-render
+  };
+
+  return (
+    <Button
+      variant="outline"
+      onClick={handleLogout}
+      className="text-primary hover:text-primary/80 bg-accent p-4 font-mono text-xl font-bold tracking-wider transition-colors"
+    >
+      <LogOut />
+    </Button>
+  );
+}
