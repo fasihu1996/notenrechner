ac60d0d7caa3d9e29a0027f8298dfa0ac2396b4a
fasihu1996 <fasih1996@gmail.com>
2025-08-01 10:21:18 +0200

    init

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-[60vh] flex-col items-center justify-center">
+      <Image
+        className="mb-5 rounded-2xl"
+        src="/notfound.gif"
+        width={500}
+        height={500}
+        alt="not found gif"
+        unoptimized
+      />
+      <p className="text-foreground mb-8 text-4xl font-bold">
+        The page you are looking for does not exist.
+      </p>
+      <Link href="/" className="text-primary underline">
+        To the journal
+      </Link>
+    </div>
+  );
+}
