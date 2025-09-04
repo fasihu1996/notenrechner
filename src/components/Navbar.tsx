0c579c63e4f045055946f077f6c6468480ea78f3
fasihu1996 <fasih1996@gmail.com>
2025-09-04 10:52:38 +0200

    font changed to Kablammo for title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,12 @@ import { createClient } from "@/utils/supabase/server";
 import LogoutButton from "./LogoutButton";
 import { Button } from "./ui/button";
 import { LogIn } from "lucide-react";
+import { Kablammo } from "next/font/google";
+
+const deliusSwashCaps = Kablammo({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 async function Navbar() {
   const supabase = await createClient();
@@ -13,32 +19,32 @@ async function Navbar() {
   } = await supabase.auth.getUser();
 
   return (
-    <nav className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
+    <nav
+      className={`bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur`}
+    >
       <div className="flex h-12 items-center justify-between px-2 sm:h-16 sm:px-4">
         <div className="flex items-center">
           <Link
             href="/"
-            className="text-primary p-2 font-mono text-lg font-bold tracking-wider sm:p-4 sm:text-2xl lg:text-3xl"
+            className={`text-primary p-2 text-4xl font-bold sm:p-4 sm:text-2xl lg:text-4xl ${deliusSwashCaps.className}`}
           >
             Notenrechner
           </Link>
         </div>
         <div className="ml-auto flex items-center justify-end space-x-1 sm:space-x-4">
           {user ? (
-            // User is logged in
             <>
-              <span className="text-primary max-w-32 truncate p-1 font-mono text-xs font-medium sm:max-w-none sm:p-4 sm:text-sm lg:text-lg">
+              <span className="text-primary max-w-32 truncate p-1 text-xs font-medium sm:max-w-none sm:p-4 sm:text-sm lg:text-lg">
                 {user.email}
               </span>
               <LogoutButton />
             </>
           ) : (
-            // User is not logged in
             <Button
-              variant="outline"
+              variant="secondary"
               size="sm"
               asChild
-              className="h-10 text-xs sm:h-10 sm:text-sm"
+              className="h-10 cursor-pointer text-xs sm:h-10 sm:text-sm"
             >
               <Link href="/login">
                 <LogIn />
